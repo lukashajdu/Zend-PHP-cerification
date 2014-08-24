7a7471a1415c9828d5156abacdf2372d968852cb
Lukas Hajdu <me@lukashajdu.com>
2014-08-24 10:35:08 +0200

    Copyright update

--- strings-and-patterns/generic-formatting.php
+++ strings-and-patterns/generic-formatting.php
@@ -2,7 +2,7 @@
 /**
  * GENERIC FORMATTING
  *
- * Copyright © 2000 Lukas Hajdu <me@lukashajdu.com>
+ * Copyright © 2014 Lukas Hajdu <me@lukashajdu.com>
  * This work is free. You can redistribute it and/or modify it under the
  * terms of the Do What The Fuck You Want To Public License, Version 2,
  * as published by Sam Hocevar. See the COPYING file for more details.
--- strings-and-patterns/string-basics.php
+++ strings-and-patterns/string-basics.php
@@ -1,14 +1,8 @@
 <?php
 /**
- * String Basics
+ * STRING BASICS
  *
- * @author: Lukas Hajdu
- */
-
-/**
- * GENERIC FORMATTING
- *
- * Copyright © 2000 Lukas Hajdu <me@lukashajdu.com>
+ * Copyright © 2014 Lukas Hajdu <me@lukashajdu.com>
  * This work is free. You can redistribute it and/or modify it under the
  * terms of the Do What The Fuck You Want To Public License, Version 2,
  * as published by Sam Hocevar. See the COPYING file for more details.
